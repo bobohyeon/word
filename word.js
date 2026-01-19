d39b0b440e4a8d649bb8bbed803f369cb31d95bb
DESKTOP-3CV852R\user1 <bo>
2026-01-19 10:20:51 +0900

    주석추가

--- word.js
+++ word.js
@@ -1,29 +1,33 @@
-function hidewords(){
+function hidewords() {
     const idEnArr = document.querySelectorAll("#idEn div");
-    console.log(idEnArr); 
- 
-    setTimeout(()=>{
+    console.log(idEnArr);
+
+    //시작버튼 누르면 5초뒤에 뒤집어지기
+    setTimeout(() => {
         for (const en of idEnArr) {
             en.classList.add("flip");
         }
-    },5000);
+    }, 5000);
 }
 
-function correctWord(){
+function correctWord() {
     const idEnArr = document.querySelectorAll("#idEn div");
     const inputArr = document.querySelectorAll(".answer input");
 
-    for(let i = 0; i<idEnArr.length; i++){
+    //단어카드와 input에 쓴 글자 -> 배열에 넣어주고
+    //배열순서대로 텍스트 비교
+    for (let i = 0; i < idEnArr.length; i++) {
         const asw = idEnArr[i].innerText;
         const userInput = inputArr[i].value;
-    
-    if(asw === userInput){
-        idEnArr[i].classList.remove("flip");
-        inputArr[i].classList.remove("wrong");
-    } else{
-        inputArr[i].classList.toggle("wrong", false);
-        inputArr[i].offsetWidth;
-        inputArr[i].classList.toggle("wrong", true);
+
+        if (asw === userInput) {
+            idEnArr[i].classList.remove("flip");
+            inputArr[i].classList.remove("wrong");
+        } else {
+            //틀렸을 때, 애니메이션처리 CSS -> 퍼옴
+            inputArr[i].classList.toggle("wrong", false);
+            inputArr[i].offsetWidth;
+            inputArr[i].classList.toggle("wrong", true);
+        }
     }
-}
 }
